b44e1b2ee040379f51f845fd334722a9b5522e97
Le-Bai <2445214364@qq.com>
2022-12-28 10:23:45 +0800

    Update theme.ts

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -8,8 +8,8 @@ export default hopeTheme({
   hostname: "https://newzone.top",
 
   author: {
-    name: "清顺",
-    url: "https://newzone.top",
+    name: "柏乐",
+    url: "https://le-bai.github.io/",
   },
 
   iconAssets: "iconfont",
@@ -34,7 +34,7 @@ export default hopeTheme({
   fullscreen: true,
 
   // 默认为 GitHub. 同时也可以是一个完整的 URL
-  repo: "rockbenben/LearnData",
+  repo: "Le-Bai/Le-Bai.io",
   // 自定义仓库链接文字。默认从 `repo` 中自动推断为 "GitHub" / "GitLab" / "Gitee" / "Bitbucket" 其中之一，或是 "Source"。
   repoLabel: "GitHub",
   // 是否在导航栏内显示仓库链接，默认为 `true`
